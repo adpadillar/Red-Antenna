4aafd6750c2f15c010b3416d7c4089f1f7eb912e
Axel Padilla <adpadillar25@gmail.com>
2024-05-20 14:23:25 -0600

    Ground Check and double jump

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -6,21 +6,22 @@ public class NewBehaviourScript : MonoBehaviour
 {
 
     private float movementInputDirection;
-
     private bool isFacingRight = true;
-
     private Rigidbody2D rb;
-    
     public float movementSpeed = 10.0f;
-
     public float jumpForce = 16.0f;
-
     public int coins = 0;
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+    private bool isGrounded;
+    private int availableJumps = 2;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        // set the ground check position to the player's feet
+        groundCheck = transform.GetChild(0);
     }
 
     // Update is called once per frame
@@ -28,11 +29,20 @@ public class NewBehaviourScript : MonoBehaviour
     {
         checkInput();
         checkMovementDirection();
+        CheckSurroundings();
     }
 
     private void FixedUpdate()
     {
         ApplyMovement();
+    }
+
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 1f, whatIsGround);
+        if (isGrounded) {
+            availableJumps = 2;
+        }
     }
 
     private void checkMovementDirection()
@@ -58,7 +68,10 @@ public class NewBehaviourScript : MonoBehaviour
 
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (isGrounded || availableJumps > 0) {
+            availableJumps--;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 
     private void ApplyMovement()
@@ -71,5 +84,11 @@ public class NewBehaviourScript : MonoBehaviour
         isFacingRight = !isFacingRight;
         transform.Rotate(0.0f, 180.0f, 0.0f);
         
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, 1f);
     }
 }
